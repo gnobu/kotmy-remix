9687c71e1c9592be906f40b66b8671175dea652d
gnobu <inyangubong17@gmail.com>
2023-12-24 23:55:42 +0100

    style: allow for label in Select

--- app/components/reusables/Select.tsx
+++ app/components/reusables/Select.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function Select({ children, containerClass, ...selectProps }: { containerClass?: string } & React.ComponentProps<'select'>) {
+export default function Select({ children, containerClass, label, ...selectProps }: { containerClass?: string, label?: string } & React.ComponentProps<'select'>) {
     return (
-        <div className={`border border-primary rounded-lg ${containerClass}`}>
-            <select className="bg-transparent focus:outline-none p-3 mr-2 cursor-pointer w-[98%]" {...selectProps}>
-                {children}
-            </select>
-        </div>
+        <label className='font-bold'>{label}
+            <div className={`border border-primary rounded-lg font-normal ${containerClass}`}>
+                <select className="bg-transparent focus:outline-none p-3 mr-2 cursor-pointer w-[98%]" {...selectProps}>
+                    {children}
+                </select>
+            </div>
+        </label>
     )
 }

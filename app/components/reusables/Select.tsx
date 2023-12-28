4a93fbe20204dd74d46c024faca5425434ffe4a8
gnobu <inyangubong17@gmail.com>
2023-12-28 13:19:32 +0100

    refactor: accept additional classnames

--- app/components/reusables/Select.tsx
+++ app/components/reusables/Select.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Select({ children, containerClass, label, ...selectProps }: { containerClass?: string, label?: string } & React.ComponentProps<'select'>) {
+export default function Select({ children, containerClass, className, label, ...selectProps }: { containerClass?: string, label?: string } & React.ComponentProps<'select'>) {
     return (
         <label className='font-bold'>{label}
-            <div className={`border border-primary rounded-lg font-normal ${containerClass}`}>
-                <select className="bg-transparent focus:outline-none p-3 mr-2 cursor-pointer w-[98%]" {...selectProps}>
+            <div className={`border border-primary rounded-lg font-normal overflow-hidden ${containerClass}`}>
+                <select className={`bg-transparent focus:outline-none p-3 mr-2 cursor-pointer w-[98%] ${className}`} {...selectProps}>
                     {children}
                 </select>
             </div>

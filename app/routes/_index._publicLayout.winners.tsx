c2ca012636ab8a23b839a11cebc7bc655a00ee17
gnobu <inyangubong17@gmail.com>
2023-12-29 23:30:15 +0100

    style: winners page coming soon

--- app/routes/_index._publicLayout.winners.tsx
+++ app/routes/_index._publicLayout.winners.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
 export default function Winners() {
   return (
-    <div>Winners</div>
+    <main className='grow'>
+      <header className="wrapper my-16 grid place-content-center">
+        <h1 className="font-satoshi-bold text-4xl text-center">Nothing to display yet...</h1>
+      </header>
+    </main>
   )
 }

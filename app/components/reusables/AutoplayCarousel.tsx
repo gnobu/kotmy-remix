bfbf46ddb24409815877ce2a73df9ac19e24f6ce
gnobu <inyangubong17@gmail.com>
2023-12-31 00:32:09 +0100

    refactor: responsive carousel timing and glitch free

--- app/components/reusables/AutoplayCarousel.tsx
+++ app/components/reusables/AutoplayCarousel.tsx
@@ -1,15 +1,28 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 type Props = {
     containerClass?: string;
     trackClass?: string;
 } & React.ComponentProps<'div'>
 
-export default function AutoplayCarousel({ children, containerClass, trackClass }: Props) {
+export default function AutoplayCarousel({ children, containerClass = '', trackClass = '' }: Props) {
+    const [fillAmount, setFillAmount] = useState(1)
+    const container = useRef<HTMLDivElement>(null)
+    const track = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        const containerWidth = container.current?.offsetWidth ?? 0
+        const trackWidth = track.current?.offsetWidth ?? 0
+        const soln = Math.min(Math.ceil(containerWidth / trackWidth))
+        container.current?.style.setProperty('--timing', `${soln * 3}s`)
+        setFillAmount(soln)
+    }, [])
     return (
-        <div className={`carousel-container ${containerClass}`}>
+        <div ref={container} className={`carousel-container ${containerClass}`}>
+            <div ref={track} className={`carousel-track ${trackClass}`}>
+                {Array(fillAmount).fill(children)}
+            </div>
             <div className={`carousel-track ${trackClass}`}>
-                {Array(10).fill(children)}
+                {Array(fillAmount).fill(children)}
             </div>
         </div>
     )

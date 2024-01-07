425304a3814438ed5a48f00b0c95937793afd368
gnobu <inyangubong17@gmail.com>
2024-01-08 00:26:03 +0100

    fix: toolbar links not working

--- app/components/admin/AdminToolbar.tsx
+++ app/components/admin/AdminToolbar.tsx
@@ -7,20 +7,18 @@ import { useState } from 'react'
 export default function AdminToolbar() {
     const [open, setOpen] = useState(false)
     return (
-        <div className="relative">
-            <div onClick={() => { setOpen(prev => !prev) }} onBlur={() => { setOpen(false) }} tabIndex={1}
-                className='p-2 rounded-full border border-primary flex items-center gap-4 cursor-pointer hover:bg-[#EEF0FF]'>
-                <div className='flex gap-3 items-center'>
-                    <img src={adminAvatar} alt="cartoon head" width={40} height={40} />
-                    <span className="grid">
-                        <span className='block text-sm font-satoshi-bold'>Admin</span>
-                        <span className='block text-xs font-satoshi-medium'>admin@kotmy.com</span>
-                    </span>
-                </div>
-                <Svg src={icons.arrowDownIcon} />
+        <div onClick={() => { setOpen(prev => !prev) }} tabIndex={0}
+            className='relative p-2 rounded-full border border-primary flex items-center gap-4 cursor-pointer hover:bg-[#EEF0FF]'>
+            <div className='flex gap-3 items-center'>
+                <img src={adminAvatar} alt="cartoon head" width={40} height={40} />
+                <span className="grid">
+                    <span className='block text-sm font-satoshi-bold'>Admin</span>
+                    <span className='block text-xs font-satoshi-medium'>admin@kotmy.com</span>
+                </span>
             </div>
+            <Svg src={icons.arrowDownIcon} />
             {open
-                ? <ul className='absolute top-[105%] bg-tertiary w-full p-2 rounded-2xl border border-primary text-xs'>
+                ? <ul className='absolute top-[105%] left-0 bg-tertiary w-full p-2 rounded-2xl border border-primary text-xs'>
                     <li><Link to={'.'} className='p-2 flex items-center gap-2 hover:bg-[#EEF0FF] rounded-lg font-satoshi-medium'>
                         <Svg src={icons.profileIcon} /> Profile
                     </Link></li>

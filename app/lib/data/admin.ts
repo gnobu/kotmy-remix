f8e6504960a20fe9f268cdccc5e3cd9e0870322f
gnobu <inyangubong17@gmail.com>
2024-01-08 15:29:19 +0100

    refactor: added needed data to admin lib

--- app/lib/data/admin.ts
+++ app/lib/data/admin.ts
@@ -1,8 +1,38 @@
+const role3 = ['edit_blog']
+const role2 = [...role3, 'edit_content']
+const role1 = [...role2, 'manage_users']
+
 export const adminUsers = [
-    { 'id': '1', 'full name': 'Admin', 'email': 'admin@gmail.com', 'username': 'admin', 'role': 'Role 1', 'access': true, },
-    { 'id': '2', 'full name': 'Nicole Clems', 'email': 'nicole@gmail.com', 'username': 'nicole', 'role': 'Role 2', 'access': false, },
-    { 'id': '3', 'full name': 'Favour Wagor', 'email': 'favour@gmail.com', 'username': 'favour', 'role': 'Role 2', 'access': true, },
-    { 'id': '4', 'full name': 'Oluchi Chinedu', 'email': 'chinedu@gmail.com', 'username': 'Oluchi', 'role': 'Role 3', 'access': false, },
-    { 'id': '5', 'full name': 'Augustine Best', 'email': 'lilklara@gmail.com', 'username': 'lilklara', 'role': 'Role 3', 'access': true, },
-    { 'id': '6', 'full name': 'Davidking Blossom', 'email': 'blossomdavid@gmail.com', 'username': 'davidking', 'role': 'Role 3', 'access': false, },
+    {
+        'id': '1', 'full name': 'Admin', 'email': 'admin@gmail.com',
+        'username': 'admin', 'role': 'Role 1', 'access': true,
+        'password': 'a12345A!', 'permissions': role1
+    },
+    {
+        'id': '2', 'full name': 'Nicole Clems', 'email': 'nicole@gmail.com',
+        'username': 'nicole', 'role': 'Role 2', 'access': false,
+        'password': 'a12345A!', 'permissions': role2
+    },
+    {
+        'id': '3', 'full name': 'Favour Wagor', 'email': 'favour@gmail.com',
+        'username': 'favour', 'role': 'Role 2', 'access': true,
+        'password': 'a12345A!', 'permissions': role2
+    },
+    {
+        'id': '4', 'full name': 'Oluchi Chinedu', 'email': 'chinedu@gmail.com',
+        'username': 'Oluchi', 'role': 'Role 3', 'access': false,
+        'password': 'a12345A!', 'permissions': role3
+    },
+    {
+        'id': '5', 'full name': 'Augustine Best', 'email': 'lilklara@gmail.com',
+        'username': 'lilklara', 'role': 'Role 3', 'access': true,
+        'password': 'a12345A!', 'permissions': role3
+    },
+    {
+        'id': '6', 'full name': 'Davidking Blossom', 'email': 'blossomdavid@gmail.com',
+        'username': 'davidking', 'role': 'Role 3', 'access': false,
+        'password': 'a12345A!', 'permissions': role3
+    },
 ]
+
+export const permissions = ['manage_users', 'edit_content', 'edit_blog']

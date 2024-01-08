4538622d9ade99bc6c10c7ebcb03b983cf7de818
gnobu <inyangubong17@gmail.com>
2024-01-08 13:16:52 +0100

    refactor: receive btn props

--- app/components/reusables/RoundButton.tsx
+++ app/components/reusables/RoundButton.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Svg from './Svg'
 
-export default function RoundButton({ icon, className = '' }: { icon: string } & React.ComponentProps<'button'>) {
+export default function RoundButton({ icon, className = '', ...props }: { icon: string } & React.ComponentProps<'button'>) {
     return (
-        <button className={`flex items-center justify-center border p-2 w-9 h-9 rounded-full ${className}`}>
+        <button {...props} className={`flex items-center justify-center border p-2 w-9 h-9 rounded-full ${className}`}>
             <Svg src={icon} />
         </button>
     )

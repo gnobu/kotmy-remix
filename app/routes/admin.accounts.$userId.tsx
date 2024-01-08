caa5c8710546abfcfff73e241b02df42641db5b7
gnobu <inyangubong17@gmail.com>
2024-01-08 15:27:46 +0100

    style: edit admin user page

---
+++ app/routes/admin.accounts.$userId.tsx
@@ -0,0 +1,61 @@
+import RoundCta from '~/components/reusables/RoundCta'
+import { Form, useLoaderData, useNavigate } from '@remix-run/react'
+import { icons } from '~/assets/icons'
+import FormControl from '~/components/reusables/FormControl'
+import Select from '~/components/reusables/Select'
+import Cta from '~/components/reusables/Cta'
+import PermissionsFormControl from '~/components/admin/PermissionsFormControl'
+import { ActionFunctionArgs, LoaderFunctionArgs, redirect } from '@remix-run/node'
+import { adminUsers, permissions } from '~/lib/data/admin'
+import { commitSession, getSession } from '~/sessions'
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  const user = adminUsers.find(user => user.id == params.userId)
+  const session = await getSession(request.headers.get("Cookie"))
+  if (!user) {
+    session.flash("alert", "error::Admin user not found")
+    return redirect('/admin/accounts', {
+      headers: { "Set-Cookie": await commitSession(session) }
+    })
+  }
+  return { permissions, user }
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData()
+  console.log(...formData)
+  console.log(formData.getAll('permission'))
+  return null
+}
+
+export default function EditAdminUser() {
+  const { permissions, user } = useLoaderData<typeof loader>()
+  const navigate = useNavigate()
+  return (
+    <main className='w-full overflow-y-auto p-6'>
+      <div className="flex items-center mb-16 gap-4">
+        <RoundCta icon={icons.arrowPrevIcon} className="hover:bg-[#F7F7F8] text-primary" onClick={() => navigate(-1)} />
+        <h1 className="text-2xl font-black text-primary">Edit User</h1>
+      </div>
+      <Form className='wrapper grid grid-cols-2 gap-6' method='post'>
+        <FormControl as='input' labelText='First Name' className='' placeholder='Enter first name' id='firstName' name='firstName' defaultValue={user['full name'].split(' ')[0]} required />
+        <FormControl as='input' labelText='Last Name' className='' placeholder='Enter last name' id='lastName' name='lastName' defaultValue={user['full name'].split(' ')[1]} required />
+        <FormControl as='input' labelText='Email Address' className='' placeholder='Enter email address' id='email' name='email' defaultValue={user.email} required />
+        <FormControl as='input' labelText='Username' className='' placeholder='Enter username' id='username' name='username' defaultValue={user.username} required />
+        <FormControl as='input' type='password' labelText='Password' className='' placeholder='Create password' id='password' name='password' defaultValue={user.password} required />
+        <Select label='Assign Role' id='role' name='role' defaultValue={user.role} required>
+          <option value="Role 1">Role 1</option>
+          <option value="Role 2">Role 2</option>
+          <option value="Role 3">Role 3</option>
+        </Select>
+
+        <PermissionsFormControl permissions={permissions} defaultPermissions={user.permissions} />
+
+        <div className='flex justify-end gap-6 col-span-2'>
+          <Cta element='button' type='reset' className='px-8 py-2 rounded-lg font-medium' variant='outline'>Reset</Cta>
+          <Cta element='button' type='submit' className='px-8 py-2 rounded-lg font-medium'>Submit</Cta>
+        </div>
+      </Form>
+    </main>
+  )
+}

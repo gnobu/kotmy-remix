e2d98d594b7733a7524b73bffb196beabed78fa8
gnobu <inyangubong17@gmail.com>
2024-01-09 21:48:43 +0100

    refactor: utilize toggletip component in admin toolbar

--- app/components/admin/AdminToolbar.tsx
+++ app/components/admin/AdminToolbar.tsx
@@ -2,12 +2,11 @@ import { adminAvatar } from '~/assets/images'
 import Svg from '../reusables/Svg'
 import { icons } from '~/assets/icons'
 import { Link } from '@remix-run/react'
-import { useState } from 'react'
+import Toggletip from '../reusables/ToggleTip'
 
 export default function AdminToolbar() {
-    const [open, setOpen] = useState(false)
-    return (
-        <div onClick={() => { setOpen(prev => !prev) }} tabIndex={0}
+    const mainComponent = (
+        <div tabIndex={0}
             className='relative p-2 rounded-full border border-primary flex items-center gap-4 cursor-pointer hover:bg-[#EEF0FF]'>
             <div className='flex gap-3 items-center'>
                 <img src={adminAvatar} alt="cartoon head" width={40} height={40} />
@@ -17,17 +16,17 @@ export default function AdminToolbar() {
                 </span>
             </div>
             <Svg src={icons.arrowDownIcon} />
-            {open
-                ? <ul className='absolute top-[105%] left-0 bg-tertiary w-full p-2 rounded-2xl border border-primary text-xs'>
-                    <li><Link to={'.'} className='p-2 flex items-center gap-2 hover:bg-[#EEF0FF] rounded-lg font-satoshi-medium'>
-                        <Svg src={icons.profileIcon} /> Profile
-                    </Link></li>
-                    <li><Link to={'/logout'} className='p-2 flex items-center gap-2 hover:bg-[#EEF0FF] rounded-lg font-satoshi-medium'>
-                        <Svg src={icons.signoutIcon} /> Sign Out
-                    </Link></li>
-                </ul>
-                : null
-            }
         </div>
+    )
+    return (
+        <Toggletip mainComponent={mainComponent}
+            childContainerClass="top-[110%] right-0 bg-tertiary p-2 border border-primary text-xs whitespace-nowrap">
+            <Link to={'.'} className='p-2 flex items-center gap-2 hover:bg-[#EEF0FF] rounded-lg font-satoshi-medium'>
+                <Svg src={icons.profileIcon} /> Profile
+            </Link>
+            <Link to={'/logout'} className='p-2 flex items-center gap-2 hover:bg-[#EEF0FF] rounded-lg font-satoshi-medium'>
+                <Svg src={icons.signoutIcon} /> Sign Out
+            </Link>
+        </Toggletip>
     )
 }

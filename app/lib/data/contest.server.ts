5cab7734a813bda6e42433de27d38692780bf1ac
gnobu <inyangubong17@gmail.com>
2024-01-12 14:20:15 +0100

    refactor: contest with stage data and types

--- app/lib/data/contest.server.ts
+++ app/lib/data/contest.server.ts
@@ -1,5 +1,5 @@
 import { contestImage1, contestImage2 } from "~/assets/images"
-import { Contest, Tournament, TournamentWContest } from "../types/contest.interface"
+import { Contest, ContestWStage, Stage, Tournament, TournamentWContest } from "../types/contest.interface"
 
 export const tournaments: Tournament[] = [
     {
@@ -35,6 +35,10 @@ export const contests: Contest[] = [
         status: 'registering',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '2',
@@ -45,6 +49,10 @@ export const contests: Contest[] = [
         status: 'ongoing',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '3',
@@ -55,6 +63,10 @@ export const contests: Contest[] = [
         status: 'completed',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '4',
@@ -65,6 +77,10 @@ export const contests: Contest[] = [
         status: 'registering',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '5',
@@ -75,6 +91,10 @@ export const contests: Contest[] = [
         status: 'ongoing',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '6',
@@ -85,6 +105,10 @@ export const contests: Contest[] = [
         status: 'completed',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '7',
@@ -95,6 +119,10 @@ export const contests: Contest[] = [
         status: 'registering',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '8',
@@ -105,6 +133,10 @@ export const contests: Contest[] = [
         status: 'ongoing',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
     {
         id: '9',
@@ -115,8 +147,110 @@ export const contests: Contest[] = [
         status: 'completed',
         start_date: new Date().toISOString(),
         end_date: new Date().toISOString(),
+        categories: {
+            "infant": "infant",
+            "newborn": "newborn"
+        },
     },
 ]
+
+
+const stages: Stage[] = [
+    {
+        "id": "646a7a56345213d4752b14b1",
+        "start_date": "2023-03-05T05:31:05.493000+00:00",
+        "end_date": "2023-10-05T05:31:05.493000+00:00",
+        "rates": {
+            "social_media": 45,
+            "judge": 0,
+            "tally": 0,
+            "givaah": 55,
+        },
+        "grade": {
+            "A": [70, 10000000],
+            "B": [60, 69],
+            "C": [50, 59],
+            "D": [40, 49],
+            "E": [20, 39],
+            "F": [0, 19],
+        },
+        "format": "STRAIGHT",
+        "active": false,
+        "board_vote": false,
+        "contest_id": "1",
+        "stage": 1,
+        "number_of_contestants": 25,
+        "total_registered_contestants": 25,
+        "status": "completed",
+        "success_count": 1000,
+        "weight": 10,
+        "contestants_upload_folder": "tournament_kotm/contest_kotm/stage_1/contestants_upload",
+        "contestants_view_folder": "tournament_kotm/contest_kotm/stage_1/contestants_view"
+    },
+    {
+        "id": "646a7e3d345213d4752b14b3",
+        "start_date": "2023-03-05T05:31:05.493000+00:00",
+        "end_date": "2023-10-05T05:31:05.493000+00:00",
+        "rates": {
+            "social_media": 45,
+            "judge": 0,
+            "tally": 0,
+            "givaah": 55,
+        },
+        "grade": {
+            "A": [70, 10000000],
+            "B": [60, 69],
+            "C": [50, 59],
+            "D": [40, 49],
+            "E": [30, 39],
+            "F": [0, 29],
+        },
+        "format": "STRAIGHT",
+        "active": true,
+        "board_vote": false,
+        "contest_id": "1",
+        "stage": 2,
+        "number_of_contestants": 6,
+        "total_registered_contestants": 25,
+        "status": "ongoing",
+        "success_count": 1000,
+        "weight": 10,
+        "contestants_upload_folder": "tournament_kotm/contest_kotm/stage_2/contestants_upload",
+        "contestants_view_folder": "tournament_kotm/contest_kotm/stage_2/contestants_view"
+    },
+    {
+        "id": "64af08140a582a136f661976",
+        "start_date": "2023-07-12T19:53:10.622000+00:00",
+        "end_date": "2023-07-12T19:53:10.622000+00:00",
+        "contest_id": "1",
+        "stage": 3,
+        "rates": {
+            "social_media": 45,
+            "judge": 0,
+            "tally": 0,
+            "givaah": 55,
+        },
+        "grade": {
+            "A": [70, 10000000],
+            "B": [60, 69],
+            "C": [50, 59],
+            "D": [40, 49],
+            "E": [20, 39],
+            "F": [0, 19],
+        },
+        "format": "STRAIGHT",
+        "active": false,
+        "board_vote": false,
+        "number_of_contestants": 4,
+        "total_registered_contestants": 25,
+        "status": "yet_to_start",
+        "success_count": 0,
+        "weight": 3,
+        "contestants_upload_folder": "tournament_kotm/contest_kotm/stage_0/contestants_upload",
+        "contestants_view_folder": "tournament_kotm/contest_kotm/stage_0/contestants_view"
+    }
+]
+
 
 export async function getTournaments(): Promise<Tournament[]> {
     return tournaments
@@ -137,6 +271,17 @@ export async function getContests(): Promise<Contest[]> {
     return contests
 }
 
+export async function getContestsWStages(): Promise<ContestWStage[]> {
+    return contests.map(contest => ({
+        ...contest,
+        stages: stages.filter(stage => stage.contest_id === contest.id)
+    }))
+}
+
 export async function getContestsInTournament(tournamentId: string): Promise<Contest[]> {
     return contests.filter(contest => contest.tournamentId === tournamentId)
+}
+
+export async function getContestsInTournamentWStages(tournamentId: string) {
+    return (await getContestsWStages()).filter(contest => contest.tournamentId === tournamentId)
 }
--- app/lib/types/contest.interface.ts
+++ app/lib/types/contest.interface.ts
@@ -1,4 +1,5 @@
 export type Status = 'registering' | 'ongoing' | 'completed'
+export type StageStatus = 'yet_to_start' | 'ongoing' | 'completed'
 
 export interface Tournament {
     id: string;
@@ -21,4 +22,30 @@ export type Contest = {
     status: Status;
     start_date: string;
     end_date: string;
+    categories: Record<string, string>;
+}
+export type ContestWStage = Contest & { stages: Stage[] }
+
+export type Grade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
+
+export type Rate = "social_media" | "tally" | "judge" | "givaah"
+
+export type Stage = {
+    id: string;
+    "start_date": string;
+    "end_date": string;
+    "weight": number;
+    "success_count": number;
+    "rates": Record<Rate, number>
+    "grade": Record<Grade, [number, number]>;
+    "format": "STRAIGHT" | 'PAIRED' | 'GROUPED';
+    "active": boolean;
+    "board_vote": boolean;
+    "contest_id": string;
+    "stage": number;
+    "number_of_contestants": number;
+    "total_registered_contestants": number;
+    "status": StageStatus;
+    "contestants_upload_folder": string;
+    "contestants_view_folder": string
 }

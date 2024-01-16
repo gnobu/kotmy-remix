14960c711e7927cc5b615c56f27f9eadbb142d28
gnobu <inyangubong17@gmail.com>
2024-01-16 21:57:38 +0100

    refactor: icon classes, common classes abstacted

--- app/components/reusables/RoundCta.tsx
+++ app/components/reusables/RoundCta.tsx
@@ -2,21 +2,25 @@ import React from 'react'
 import Svg from './Svg'
 import { Link } from '@remix-run/react'
 import type { RemixLinkProps } from '@remix-run/react/dist/components'
+import cn from 'classnames'
 
 type RoundCtaProps = (
     | ({ element?: 'button'; } & React.ButtonHTMLAttributes<HTMLButtonElement>)
     | ({ element: 'link'; } & RemixLinkProps)
-) & { icon: string }
+) & { icon: string, iconClass?: string }
 
-export default function RoundCta({ icon, className = '', ...props }: RoundCtaProps) {
+export default function RoundCta({ icon, className = '', iconClass = '', ...props }: RoundCtaProps) {
+    const classNames = cn(`flex items-center justify-center border min-w-[32px] min-h-[32px] rounded-full ${className}`, {
+        'bg-slate-100 border-slate-400 text-slate-400 cursor-not-allowed': props['aria-disabled']
+    })
     if (props.element === 'link') {
-        return <Link  {...props} className={`flex items-center justify-center border p-2 w-9 h-9 rounded-full ${className}`}>
-            <Svg src={icon} />
+        return <Link  {...props} className={classNames}>
+            <Svg src={icon} className={iconClass} />
         </Link>
     }
     return (
-        <button {...props} className={`flex items-center justify-center border p-2 w-9 h-9 rounded-full ${className}`}>
-            <Svg src={icon} />
+        <button {...props} className={classNames}>
+            <Svg src={icon} className={iconClass} />
         </button>
     )
 }

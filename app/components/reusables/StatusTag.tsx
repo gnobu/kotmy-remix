6d9cfd9b40ff0c99882fe335ac665a93f68d6694
gnobu <inyangubong17@gmail.com>
2024-01-17 23:30:23 +0100

    fix: status tag colors not showing

--- app/components/reusables/StatusTag.tsx
+++ app/components/reusables/StatusTag.tsx
@@ -1,14 +1,19 @@
 import cn from 'classnames'
 
-type Color = 'red' | 'yellow' | 'green'
 type StatusTagProps = { status: string, className?: string }
 
 const safe = ['ongoing', 'verified']
+const caution = ['registering', 'pending']
 const danger = ['completed', 'revoked']
 
 export default function StatusTag({ status, className }: StatusTagProps) {
-    const color: Color = safe.includes(status) ? 'green' : danger.includes(status) ? 'red' : 'yellow'
-    return <span className={cn(`w-fit px-4 pl-7 py-1.5 bg-${color}-100  text-${color}-700 rounded-md text-sm capitalize font-satoshi-medium flex items-center ${className}`)}>
-        <span className={`before:content-['•'] before:absolute relative before:-left-4 before:top-[10%] before:bg-${color}-700 before:text-2xl before:leading-3`}>{status}</span>
+    return <span className={cn(`w-fit px-4 pl-7 py-1.5 rounded-md text-sm capitalize font-satoshi-medium flex items-center ${className}`, {
+        'bg-green-100 text-green-700': safe.includes(status),
+        'bg-yellow-100 text-yellow-700': caution.includes(status),
+        'bg-red-100 text-red-700': danger.includes(status),
+    })}>
+        <span className={`before:content-['•'] before:absolute relative before:-left-4 before:top-[10%] before:text-2xl before:leading-3`}>
+            {status}
+        </span>
     </span>
 }

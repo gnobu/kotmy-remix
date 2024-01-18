12a0e5d40975c7c3db9fea615118253c2e4d111e
gnobu <inyangubong17@gmail.com>
2024-01-18 14:28:18 +0100

    style: added kotmy logo to mobile header

--- app/components/admin/MobileHeader.tsx
+++ app/components/admin/MobileHeader.tsx
@@ -1,17 +1,22 @@
 import { icons } from "~/assets/icons"
 import Svg from "../reusables/Svg"
+import { Link } from "@remix-run/react"
+import { logo } from "~/assets/images"
 
 export default function MobileHeader({ toggleNav }: { toggleNav: () => void }) {
     return (
-        <div className="flex justify-between items-center p-4 border-b">
-            <p className="font-medium text-primary text-xs">
-                <span className="block text-base font-satoshi-bold">Hello Admin</span>
-                <span className="block">Welcome back to KOTMY 👋</span>
+        <div className="flex items-center gap-4 p-4 border-b">
+            <Link to={'/'}>
+                <img src={logo} alt="KOTMY" />
+            </Link>
+            <p className="grid font-medium text-primary text-xs">
+                <span className="text-base font-satoshi-bold line-clamp-1">Hello Admin</span>
+                <span className="line-clamp-1">Welcome back to KOTMY 👋</span>
             </p>
             <button
                 onClick={toggleNav}
                 title='open Menu'
-                className="flex items-center justify-center rounded p-2 px-1 hover:outline outline-primary"
+                className="ml-auto flex items-center justify-center rounded p-2 px-1 hover:outline outline-primary"
             >
                 <Svg src={icons.adminHamburgerIcon} width={30} height={24} />
             </button>

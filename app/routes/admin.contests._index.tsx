8600099737ccac028475c33bf558d360f84bf7a9
gnobu <inyangubong17@gmail.com>
2024-01-19 22:17:34 +0100

    style: mobile contests page

--- app/routes/admin.contests._index.tsx
+++ app/routes/admin.contests._index.tsx
@@ -23,14 +23,14 @@ export default function Contests() {
     const { contests } = useLoaderData<typeof loader>()
     return (
         <main className='w-full overflow-y-auto p-6'>
-            <section className="flex justify-between items-center mb-16">
+            <section className="flex max-sm:flex-col gap-10 justify-between sm:items-center mb-6 sm:mb-16">
                 <h1 className="text-2xl font-black text-primary">Contests</h1>
-                <Cta element="link" to='add' className="flex gap-2 items-center rounded-lg px-3 py-2">
+                <Cta element="link" to='add' className="flex gap-2 items-center justify-center rounded-lg px-3 py-2">
                     <Svg src={icons.addIcon} width={'.9em'} />
                     Create Contest
                 </Cta>
             </section>
-            <section className='my-12'>
+            <section className='my-6 sm:my-12'>
                 <ContestTable data={contests} />
             </section>
         </main>

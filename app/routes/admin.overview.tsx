7a2f17fa248783d0f6d1e767dfcfd7e5df49c1e0
gnobu <inyangubong17@gmail.com>
2024-01-22 10:47:06 +0100

    style: admin home title

--- app/routes/admin.overview.tsx
+++ app/routes/admin.overview.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
     return (
         <main className='w-full overflow-y-auto p-6'>
-            <h1 className="text-2xl font-black text-primary">Home</h1>
+            <h1 className="grid font-medium text-primary">
+                <span className="text-xl sm:text-2xl font-satoshi-bold line-clamp-1">Hello Admin</span>
+                <span className="line-clamp-1">Welcome back to KOTMY 👋</span>
+            </h1>
         </main>
     )
 }

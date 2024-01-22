8902a548fec3a3aee8f3e491ce8efc39c58a5ac0
gnobu <inyangubong17@gmail.com>
2024-01-22 19:23:58 +0100

    style: hover state for primary cta

--- app/components/reusables/Cta.tsx
+++ app/components/reusables/Cta.tsx
@@ -15,7 +15,7 @@ export default function Cta({ variant = 'solid', kind = 'primary', ...props }: C
     if (props.element === 'button') {
         return <button {...props} className={cn(`border whitespace-nowrap text-center ${props.className}`, {
             'border-disabled text-inherit': props.disabled,
-            'bg-accent border-accent text-secondary': variant === 'solid',
+            'bg-accent border-accent hover:bg-accent/90 text-secondary': variant === 'solid',
             'bg-red-600 border-red-600 hover:bg-red-400 text-secondary': variant === 'solid' && kind === 'danger',
             'bg-gray-300 border-disabled': variant === 'solid' && props.disabled,
             'text-accent border-accent border-2': variant === 'outline',
@@ -24,7 +24,7 @@ export default function Cta({ variant = 'solid', kind = 'primary', ...props }: C
         })}>{props.children}</button>
     }
     return <Link {...props} className={cn(`border whitespace-nowrap text-center ${props.className}`, {
-        'bg-accent border-accent text-secondary': variant === 'solid',
+        'bg-accent border-accent hover:bg-accent/90 text-secondary': variant === 'solid',
         'text-accent border-accent border-2': variant === 'outline',
         'border-red-400': kind === 'danger',
         'text-red-400': kind === 'danger',

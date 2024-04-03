9b52b3f8aceb5ccf0b1fca1a96e5925b1e72c6b9
gnobu <inyangubong17@gmail.com>
2024-04-03 16:26:45 +0100

    fix: empty string contest image not showing

--- app/components/reusables/ContestCard.tsx
+++ app/components/reusables/ContestCard.tsx
@@ -20,7 +20,7 @@ export default function ContestCard({ contest, to, withTag, withCategory }: Prop
                 ? 'red' : 'gray'
     return (
         <Link to={to} className="flex flex-col gap-2 max-w-lg relative w-full">
-            <img src={contest.image ?? noImage} alt="contest image" className="rounded-3xl h-56 object-cover" />
+            <img src={contest.image || noImage} alt="contest image" className="rounded-3xl h-56 object-cover" />
             {withTag ? <StatusTag status={contest.status} className="absolute top-4 left-4" color={color} /> : null}
             {withCategory ? <span className="text-sm">Category</span> : null}
             <p className="text-2xl font-bold capitalize">{contest.name}</p>

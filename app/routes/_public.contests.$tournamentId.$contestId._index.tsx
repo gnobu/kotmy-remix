6708c659f532a7fafc2737f138830ca991fa7c89
gnobu <inyangubong17@gmail.com>
2024-04-04 13:01:21 +0100

    feat: fetch public single contest

--- app/routes/_public.contests.$tournamentId.$contestId._index.tsx
+++ app/routes/_public.contests.$tournamentId.$contestId._index.tsx
@@ -1,14 +1,15 @@
 import { LoaderFunctionArgs, json, redirect } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
+
+import { contestRepo } from "~/models/contest/contest.server"
 import OngoingContest from "~/components/public/contests/OngoingContest"
 import RegisteringContest from "~/components/public/contests/RegisteringContest"
-import { getContest } from "~/lib/data/contest.server"
 
 export async function loader({ params }: LoaderFunctionArgs) {
     const { tournamentId, contestId } = params
     if (!contestId) return redirect(`/contests/${tournamentId}`)
-    const contest = await getContest(contestId)
-    if (!contest) return redirect(`/contests/${tournamentId}`)
+    const { data: contest, error } = await contestRepo.getContestById(contestId)
+    if (error) return redirect(`/contests/${tournamentId}`)
     return json({ contest })
 }
 

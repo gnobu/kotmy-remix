d463705a38f209a017dfe3e424dfbb6b278fdcb9
gnobu <inyangubong17@gmail.com>
2024-05-24 09:36:35 +0100

    feat: tally vote on main stage page

--- app/components/public/contests/ContestantCard.tsx
+++ app/components/public/contests/ContestantCard.tsx
@@ -1,7 +1,8 @@
+import { IContestant } from '~/models/contestant/types/contestant.interface'
 import { noImage } from '~/assets/images'
 import SocialLink from './SocialLink'
 import { Social } from '~/models/contest/types/contest.interface'
-import { IContestant } from '~/models/contestant/types/contestant.interface'
+import TallyVoteDialog from './TallyVoteDialog'
 
 export default function ContestantCard({ contestant, socialMedia }: { contestant: IContestant, socialMedia: Social }) {
     const fullName = `${contestant.contestant_biodata.first_name} ${contestant.contestant_biodata.last_name}`
@@ -13,7 +14,9 @@ export default function ContestantCard({ contestant, socialMedia }: { contestant
                 <span className='block font-black uppercase mb-4'>{fullName}</span>
                 <div className="grid grid-cols-2 gap-4">
                     <SocialLink type={socialMedia} url={contestant.social_media_url} />
-                    <SocialLink type='tally' url='.' />
+                    <TallyVoteDialog contestant={contestant}>
+                        <SocialLink type='tally' className='w-full' />
+                    </TallyVoteDialog>
                     {/* <SocialLink type='givaah' url='.' /> */}
                 </div>
             </div>
--- app/routes/_public.contests.$tournamentId.$contestId._index.tsx
+++ app/routes/_public.contests.$tournamentId.$contestId._index.tsx
@@ -1,8 +1,8 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node"
 import { useRouteLoaderData } from "@remix-run/react"
 
+import { getTallyLink, registerContestant } from "~/models/contestant/actions.server"
 import { setToast } from "~/lib/session.server"
-import { contestantRepo } from "~/models/contestant/contestant.server"
 import OngoingContest from "~/components/public/contests/OngoingContest"
 import RegisteringContest from "~/components/public/contests/RegisteringContest"
 import { StageContestantsLoader } from "./_public.contests.$tournamentId.$contestId"
@@ -12,6 +12,7 @@ export async function action({ request }: LoaderFunctionArgs) {
     const formData = await request.formData()
     const intent = formData.get("intent") as "register" | "tally_vote"
     if (intent === "register") return await registerContestant(formData, request)
+    if (intent === "tally_vote") return await getTallyLink(formData, request)
     const { headers } = await setToast({ request, toast: 'error::This action is not yet supported' })
     return json(null, { headers })
 }
@@ -22,8 +23,8 @@ export default function ContestPage() {
     if (!stageContestants) throw new Error("Could not load stage contestants")
     const { contest, stage } = stageContestants
     return (
-        <main className='grow'>
-            {contest.status === 'registering'
+        <main className="grow">
+            {contest.status === "registering"
                 ? <RegisteringContest contest={contest} />
                 : <OngoingContest contest={contest} stage={stage} />
             }

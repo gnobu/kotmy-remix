1a3f75931e3c0e2aa8a95160aebdb1e43e2f21ff
gnobu <inyangubong17@gmail.com>
2024-05-24 09:34:53 +0100

    refactor: social link to act like vote link

--- app/components/public/contests/SocialLink.tsx
+++ app/components/public/contests/SocialLink.tsx
@@ -1,32 +1,40 @@
-import { Link } from "@remix-run/react"
-import cn from "classnames"
+import React from "react"
+
+import { cn } from "~/lib/utils"
+import Cta from "~/components/reusables/Cta"
 import Svg from "~/components/reusables/Svg"
 import { socialIcons } from "~/lib/data/socials"
 
 type Props = {
-    type: keyof typeof socialIcons;
-    url: string;
+    type: keyof typeof socialIcons,
+    url?: string,
+    className?: string,
 }
 
-export default function SocialLink({ type, url }: Props) {
+export default React.forwardRef(function SocialLink(
+    { type, url, className = "", ...rest }: Props,
+    ref: React.ForwardedRef<HTMLButtonElement>) {
+    const props = url
+        ? { element: "link", to: url, ...rest } as const
+        : { element: "button", ref, ...rest } as const
     return (
-        <Link to={url} className={cn(`p-2 flex items-center gap-2 border rounded-full`, {
-            'border-facebook text-facebook bg-facebookBG': type === 'facebook',
-            'border-instagram text-instagram bg-instagramBG': type === 'instagram',
-            'border-twitter text-twitter bg-twitterBG': type === 'twitter',
-            'border-tally text-tally bg-tallyBG': type === 'tally',
-            'border-givaah text-givaah bg-givaahBG': type === 'givaah',
-        })}>
-            <span className={cn(`w-6 h-6 flex items-center justify-center rounded-full p-1`, {
-                'bg-facebook': type === 'facebook',
-                'bg-instagram': type === 'instagram',
-                'bg-twitter': type === 'twitter',
-                'bg-tally': type === 'tally',
-                'bg-givaah': type === 'givaah',
+        <Cta {...props} variant="outline" className={cn("p-2 flex items-center border rounded-full", {
+            "border-facebook text-facebook bg-facebookBG hover:bg-facebook/15": type === "facebook",
+            "border-instagram text-instagram bg-instagramBG hover:bg-instagram/15": type === "instagram",
+            "border-twitter text-twitter bg-twitterBG hover:bg-twitter/15": type === "twitter",
+            "border-tally text-tally bg-tallyBG hover:bg-tally/15": type === "tally",
+            "border-givaah text-givaah bg-givaahBG hover:bg-givaah/15": type === "givaah",
+        }, className)}>
+            <span className={cn("w-6 h-6 flex items-center justify-center rounded-full p-1", {
+                "bg-facebook": type === "facebook",
+                "bg-instagram": type === "instagram",
+                "bg-twitter": type === "twitter",
+                "bg-tally": type === "tally",
+                "bg-givaah": type === "givaah",
             })}>
                 <Svg src={socialIcons[type]} />
             </span>
-            <span className="text-xs font-bold"><span className="capitalize">{type}</span> vote</span>
-        </Link>
+            <span className="grow text-xs font-bold text-center mr-2"><span className="capitalize">{type}</span> vote</span>
+        </Cta>
     )
-}
+})

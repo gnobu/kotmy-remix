236eafe3d37eff86cbab41a695b39ab8bd1cc9d8
gnobu <inyangubong17@gmail.com>
2024-05-24 09:31:01 +0100

    refactor: default grade to F

--- app/components/public/contests/Grade.tsx
+++ app/components/public/contests/Grade.tsx
@@ -1,25 +1,26 @@
-import classNames from 'classnames'
+import { cn } from "~/lib/utils"
 
 export default function Grade({ grade }: { grade: string }) {
+    const cleanedGrade = grade ? grade.toUpperCase() : "F"
     return (
-        <div className='grid grid-cols-6 rounded-md overflow-hidden text-white text-xs font-black'>
-            <div className={classNames('bg-grade-F h-full px-2 py-1 text-center')}>
-                {grade === 'F' ? grade : null}
+        <div className="grid grid-cols-6 rounded-md overflow-hidden text-white text-xs font-black">
+            <div className={cn("bg-grade-F h-full px-2 py-1 text-center")}>
+                {cleanedGrade === "F" ? cleanedGrade : null}
             </div>
-            <div className={classNames('h-full px-2 py-1 text-center', { 'bg-grade-E': grade <= 'E', 'bg-grade-Ea': grade > 'E' })}>
-                {grade === 'E' ? grade : null}
+            <div className={cn("h-full px-2 py-1 text-center", { "bg-grade-E": cleanedGrade <= "E", "bg-grade-Ea": cleanedGrade > "E" })}>
+                {cleanedGrade === "E" ? cleanedGrade : null}
             </div>
-            <div className={classNames('h-full px-2 py-1 text-center', { 'bg-grade-D': grade <= 'D', 'bg-grade-Da': grade > 'D' })}>
-                {grade === 'D' ? grade : null}
+            <div className={cn("h-full px-2 py-1 text-center", { "bg-grade-D": cleanedGrade <= "D", "bg-grade-Da": cleanedGrade > "D" })}>
+                {cleanedGrade === "D" ? cleanedGrade : null}
             </div>
-            <div className={classNames('h-full px-2 py-1 text-center', { 'bg-grade-C': grade <= 'C', 'bg-grade-Ca': grade > 'C' })}>
-                {grade === 'C' ? grade : null}
+            <div className={cn("h-full px-2 py-1 text-center", { "bg-grade-C": cleanedGrade <= "C", "bg-grade-Ca": cleanedGrade > "C" })}>
+                {cleanedGrade === "C" ? cleanedGrade : null}
             </div>
-            <div className={classNames('h-full px-2 py-1 text-center', { 'bg-grade-B': grade <= 'B', 'bg-grade-Ba': grade > 'B' })}>
-                {grade === 'B' ? grade : null}
+            <div className={cn("h-full px-2 py-1 text-center", { "bg-grade-B": cleanedGrade <= "B", "bg-grade-Ba": cleanedGrade > "B" })}>
+                {cleanedGrade === "B" ? cleanedGrade : null}
             </div>
-            <div className={classNames('h-full px-2 py-1 text-center', { 'bg-grade-A': grade <= 'A', 'bg-grade-Aa': grade > 'A' })}>
-                {grade === 'A' ? grade : null}
+            <div className={cn("h-full px-2 py-1 text-center", { "bg-grade-A": cleanedGrade <= "A", "bg-grade-Aa": cleanedGrade > "A" })}>
+                {cleanedGrade === "A" ? cleanedGrade : null}
             </div>
         </div>
     )
